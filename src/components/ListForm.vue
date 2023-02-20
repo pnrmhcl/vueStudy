1fe838f25e5ab4a7c73710c475916c8e8d720eb6
pnrmhcl <pmihcioglu@gmail.com>
2023-02-20 16:36:54 +0300

    fix: list form css

--- src/components/ListForm.vue
+++ src/components/ListForm.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    <label>Pınar</label>
+  <div id="1" class="container">
+    <div class="form">
+      <div class="d-grid gap-2"></div>
+    </div>
   </div>
 </template>
+
+<style>
+.form {
+  background-color: #ffff;
+  padding: 1em;
+  margin-bottom: 1em;
+  width: 50%;
+}
+
+.container {
+  width: 100%;
+  background-color: #ffff;
+  border-radius: 20px 20px 20px 20px;
+}
+</style>

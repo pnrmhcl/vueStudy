2365a67a3cc1d4ce9cab463565961f1d4c43d650
pnrmhcl <pmihcioglu@gmail.com>
2023-02-20 16:42:16 +0300

    feature: list form data

--- src/components/ListForm.vue
+++ src/components/ListForm.vue
@@ -1,19 +1,28 @@
 <template>
   <div id="1" class="container">
-    <div class="form">
-      <div class="d-grid gap-2"></div>
+    <div class="d-grid gap-2">
+      <div>
+        <b-table striped hover :items="items"></b-table>
+      </div>
     </div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { age: 40, first_name: "Dickerson", last_name: "Macdonald" },
+        { age: 21, first_name: "Larsen", last_name: "Shaw" },
+        { age: 89, first_name: "Geneva", last_name: "Wilson" },
+        { age: 38, first_name: "Jami", last_name: "Carney" },
+      ],
+    };
+  },
+};
+</script>
 <style>
-.form {
-  background-color: #ffff;
-  padding: 1em;
-  margin-bottom: 1em;
-  width: 50%;
-}
-
 .container {
   width: 100%;
   background-color: #ffff;

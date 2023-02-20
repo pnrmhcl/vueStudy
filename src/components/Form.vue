23467adbf3f6ba05474d72315f5bac3b8b76a9fd
pnrmhcl <pmihcioglu@gmail.com>
2023-02-20 14:12:46 +0300

    feature: user form

---
+++ src/components/Form.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="container">
+    <div class="form">
+      <h1>Add User</h1>
+      <div class="input">
+        <b-form-group
+          id="fieldset-1"
+          label="Firstname"
+          label-for="input-1"
+          valid-feedback="Thank you!"
+          :invalid-feedback="invalidFeedback"
+          :state="state"
+        >
+          <b-form-input
+            id="input-1"
+            v-model="name"
+            :state="state"
+            trim
+          ></b-form-input>
+        </b-form-group>
+      </div>
+      <div class="input">
+        <b-form-group
+          id="fieldset-2"
+          label="Lastname"
+          label-for="input-2"
+          valid-feedback="Thank you!"
+          :invalid-feedback="invalidFeedback"
+          :state="state"
+        >
+          <b-form-input
+            id="input-2"
+            v-model="lastname"
+            :state="state"
+            trim
+          ></b-form-input>
+        </b-form-group>
+      </div>
+      <div class="input">
+        <b-form-group
+          id="fieldset-3"
+          label="Age"
+          label-for="input-3"
+          valid-feedback="Thank you!"
+          :invalid-feedback="invalidFeedback"
+          :state="state"
+        >
+          <b-form-input
+            id="input-3"
+            v-model="age"
+            :state="state"
+            trim
+          ></b-form-input>
+        </b-form-group>
+      </div>
+      <div>
+        <b-button block variant="primary">Confirm</b-button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "UserForm",
+  computed: {
+    state() {
+      console.log(this);
+      return this.name.length >= 4;
+    },
+    invalidFeedback() {
+      if (this.name.length > 0) {
+        return "Enter at least 4 characters.";
+      }
+      return "Please enter something.";
+    },
+  },
+  data() {
+    return {
+      name: "",
+      lastname: "",
+      age: "",
+    };
+  },
+};
+</script>
+<style>
+.form {
+  background-color: #ffff;
+  padding: 1em;
+  margin-bottom: 1em;
+  width: 50%;
+}
+.input {
+  text-align: justify;
+}
+.container {
+  width: 100%;
+  background-color: #ffff;
+}
+</style>
